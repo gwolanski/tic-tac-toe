6d723ae41b88be5eecfc49f390c294c6f4947b2e
gwolanski <gwol@udel.edu>
2023-05-23 07:44:19 -0600

    Logic to determine winner and tie.

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ const gameController = (() => {
 
     let activePlayer = player1;
 
+    let messageDisplay = document.getElementById('message');
+    messageDisplay.innerHTML = `${activePlayer.name}'s turn`;
+    messageDisplay.classList.add('playerOne');
+
     console.log(activePlayer.marker)
 
     return {
         activePlayer,
         player1,
-        player2
+        player2,
+        messageDisplay
     }
     
 })()
@@ -23,17 +28,27 @@ const gameController = (() => {
 
 
 
-function playerTurn(cell) {
-    //if cell already contains a mark, do nothing
-    //if cell if empty, add mark for the current player's turn. 
-    //then run a function that checks for a winner
-    //then run change the activePlayer to the other player
+function playerTurn(cell, cellIndex) {
     if (cell.innerHTML == "") {
-        console.log("empty");
-        switchPlayer();
+        gameBoard.board[cellIndex] = `${gameController.activePlayer.marker}`;
+        updateDisplay(gameBoard.board);
 
+        const noMoreMoves = !gameBoard.board.includes("");
+
+        let winner = winnerCheck(gameBoard.board);
+        if (winner) {
+            console.log('winner!');
+        } else if (noMoreMoves) {
+            console.log('TIE');
+        } else {
+            switchPlayer();
+        }
+        console.log("winner: " + winner);
+
+        
     } else {
-        console.log("full");
+        //if cell already contains a mark, do nothing
+        console.log("cell is full");
     }
     
 }
@@ -41,22 +56,27 @@ function playerTurn(cell) {
 function switchPlayer() {
     if (gameController.activePlayer == gameController.player1) {
         gameController.activePlayer = gameController.player2;
+        gameController.messageDisplay.innerHTML = "Player 2's turn";
+        gameController.messageDisplay.classList.remove('playerOne');
+        gameController.messageDisplay.classList.add('playerTwo');
         console.log("activePlayer: " + gameController.activePlayer.name)
     } else if (gameController.activePlayer == gameController.player2) {
         gameController.activePlayer = gameController.player1;
+        gameController.messageDisplay.innerHTML = "Player 1's turn";
+        gameController.messageDisplay.classList.remove('playerTwo');
+        gameController.messageDisplay.classList.add('playerOne');
         console.log("activePlayer: " + gameController.activePlayer.name)
     }
-    }
+}
 
-
-//create a gameboard using the module pattern
-const gameBoard = (() => {
+function updateDisplay(board) {
     const numRows = 3;
     const numColumns = 3;
     const boardSize = numRows * numColumns;
-    const board = ["X", "O", "X", "O", "X", "O", "X", "X", "O"];
-
+    
     let boardContainer = document.getElementById('gameboard');
+
+    boardContainer.innerHTML = "";
 
     boardContainer.style.gridTemplateColumns = `repeat(3, 125px)`;
     boardContainer.style.gridTemplateRows = `repeat(3, 125px)`;
@@ -65,9 +85,61 @@ const gameBoard = (() => {
         let cell = document.createElement('div');
         cell.className = 'cell';
         cell.innerHTML = board[i];
+        cell.dataset.index = i;
+        let cellIndex = cell.dataset.index;
         cell.addEventListener('click', function(){
-            playerTurn(cell);
+            playerTurn(cell, cellIndex);
         });
         boardContainer.appendChild(cell);
+        
     }
+}
+
+
+//create a gameboard using the module pattern
+const gameBoard = (() => {
+
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    updateDisplay(board);
+
+    return {board}
 })();
+
+function winnerCheck(board) {
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    //see if the board contains the same marker 3x in a row on the gameboard
+    //if it does, change the message to show the game winner
+
+    for (const combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (board[a] == board[b] && board[b] == board[c] && board[a] !== '') {
+            console.log("board[a]: " + board[a])
+            return board[a];
+        } 
+    }
+    return null
+
+    
+
+    
+}
+
+// const winner = winnerCheck(gameBoard.board);
+//     console.log("winner: " + winner);
+//     if (winner) {
+//         console.log(`Player ${winner} wins!`)
+//     } else {
+//         console.log("It's a tie!");
+//     }
+
