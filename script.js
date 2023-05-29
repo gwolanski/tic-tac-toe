1e04b9d76980aa481c6099a22039dc448472848f
gwolanski <gwol@udel.edu>
2023-05-29 08:24:22 -0600

    Reset button

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ const gameController = (() => {
     messageDisplay.innerHTML = `${activePlayer.name}'s turn`;
     messageDisplay.classList.add('playerOne');
 
-    console.log(activePlayer.marker)
+    let resetButton = document.getElementById('reset');
+    function resetGame() {
+    gameBoard.board = ["", "", "", "", "", "", "", "", ""];
+
+    updateDisplay(gameBoard.board);
+    }
+    resetButton.addEventListener('click', resetGame);
 
     return {
         activePlayer,
@@ -25,7 +31,6 @@ const gameController = (() => {
     
 })()
 
-
 function playerTurn(cell, cellIndex) {
     if (cell.innerHTML == "") {
         gameBoard.board[cellIndex] = `${gameController.activePlayer.marker}`;
@@ -33,45 +38,65 @@ function playerTurn(cell, cellIndex) {
 
         const noMoreMoves = !gameBoard.board.includes("");
 
-        let winner = winnerCheck(gameBoard.board);
+        let winnerMark = winnerCheck(gameBoard.board);
+        let winner;
+        if (winnerMark == gameController.player1.marker) {
+            winner = "Player 1";
+        } else if (winnerMark == gameController.player2.marker) {
+            winner = "Player 2";
+        }
+
         let cells = document.getElementsByClassName("cell");
+        let messageDiv = document.getElementById("message");
 
-
-        console.log("cells: " + cells)
-        if (winner) {
-            console.log('winner!');
-            
+        if (winnerMark) {
             for (let i = 0; i < 9; i++) {
                 cells[i].replaceWith(cells[i].cloneNode(true));
             }
+            messageDiv.innerHTML = `${winner} (${winnerMark}) wins!`;
+            messageDiv.setAttribute("id", "winner");
         } else if (noMoreMoves) {
-            console.log('TIE');
+            messageDiv.innerHTML = "It's a tie!"
         } else {
             switchPlayer();
         }
-        console.log("winner: " + winner);
-
-        
-    } else {
-        //if cell already contains a mark, do nothing
-        console.log("cell is full");
     }
     
-}
+    function switchPlayer() {
+        if (gameController.activePlayer == gameController.player1) {
+            gameController.activePlayer = gameController.player2;
+            gameController.messageDisplay.innerHTML = "Player 2's turn";
+            gameController.messageDisplay.classList.remove('playerOne');
+            gameController.messageDisplay.classList.add('playerTwo');
+        } else if (gameController.activePlayer == gameController.player2) {
+            gameController.activePlayer = gameController.player1;
+            gameController.messageDisplay.innerHTML = "Player 1's turn";
+            gameController.messageDisplay.classList.remove('playerTwo');
+            gameController.messageDisplay.classList.add('playerOne');
+        }
+    }
 
-function switchPlayer() {
-    if (gameController.activePlayer == gameController.player1) {
-        gameController.activePlayer = gameController.player2;
-        gameController.messageDisplay.innerHTML = "Player 2's turn";
-        gameController.messageDisplay.classList.remove('playerOne');
-        gameController.messageDisplay.classList.add('playerTwo');
-        console.log("activePlayer: " + gameController.activePlayer.name)
-    } else if (gameController.activePlayer == gameController.player2) {
-        gameController.activePlayer = gameController.player1;
-        gameController.messageDisplay.innerHTML = "Player 1's turn";
-        gameController.messageDisplay.classList.remove('playerTwo');
-        gameController.messageDisplay.classList.add('playerOne');
-        console.log("activePlayer: " + gameController.activePlayer.name)
+    function winnerCheck(board) {
+        const winningCombos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+    
+        for (const combo of winningCombos) {
+            const [a, b, c] = combo;
+            if (board[a] == board[b] && board[b] == board[c] && board[a] !== '') {
+                console.log("board[a]: " + board[a])
+                return board[a];
+            } 
+        }
+        return null
+    
     }
 }
 
@@ -103,8 +128,6 @@ function updateDisplay(board) {
     }
 }
 
-
-//create a gameboard using the module pattern
 const gameBoard = (() => {
 
     const board = ["", "", "", "", "", "", "", "", ""];
@@ -113,29 +136,3 @@ const gameBoard = (() => {
 
     return {board}
 })();
-
-function winnerCheck(board) {
-    const winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ]
-
-    //see if the board contains the same marker 3x in a row on the gameboard
-    //if it does, change the message to show the game winner
-
-    for (const combo of winningCombos) {
-        const [a, b, c] = combo;
-        if (board[a] == board[b] && board[b] == board[c] && board[a] !== '') {
-            console.log("board[a]: " + board[a])
-            return board[a];
-        } 
-    }
-    return null
-
-}
